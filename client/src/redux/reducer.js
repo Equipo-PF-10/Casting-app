875819b538dc32ea90b9dd3d4c9c3ed691b5eacd
Edward Vera <edwardismael1@gmail.com>
2023-07-18 19:42:09 -0300

    hago dispatch de la action register_model con los input ingresados del registro

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,28 +1,10 @@
-import { ERROR, REGISTER_MODEL } from "./actions.js";
 
-const initialState = {
-  models: [],
-  errors: {},
-};
 
-const rootReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-
-    /*
-    case REGISTER_MODEL:
-            return {
-                ...state,
-                messageRegistered: payload
-            }
-    */
-    case ERROR:
-      return {
-        ...state,
-        errors: payload,
-      };
-    default:
-      return { ...state };
-  }
+const rootReducer=() => {
+  //switch(action.type) {
+      //default:
+      //    return
+  //}
 };
 
 export default rootReducer;
