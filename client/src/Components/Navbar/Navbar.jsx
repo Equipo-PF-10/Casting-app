2b5eb4e28c8ad2f38f34719bca918b8c8334948a
JulioACegarraC <juliocegarra19@gmail.com>
2023-07-18 01:01:01 -0500

    maqueteado del navbar y la landing page, pendiente definir el tipo de letras y agregar efectos

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import './NavbarModule.css'
+
+function Navbar() {
+
+    <nav>
+        <svg width="100px" height="100px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#9eedef">
+            <g id="SVGRepo_bgCarrier" stroke-width="0" transform="translate(0,0), scale(1)">
+                <path transform="translate(-2.4, -2.4), scale(1.7999999999999998)" fill="#608080" d="M9.166.33a2.25 2.25 0 00-2.332 0l-5.25 3.182A2.25 2.25 0 00.5 5.436v5.128a2.25 2.25 0 001.084 1.924l5.25 3.182a2.25 2.25 0 002.332 0l5.25-3.182a2.25 2.25 0 001.084-1.924V5.436a2.25 2.25 0 00-1.084-1.924L9.166.33z" strokewidth="0"/>
+            </g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#9eedef" stroke-width="0.144"/>
+            <g id="SVGRepo_iconCarrier">
+                <circle cx="9" cy="6" r="4" stroke="#f5f5f5" stroke-width="1.7280000000000002"/>
+                <path opacity="0.5" d="M12.5 4.3411C13.0375 3.53275 13.9565 3 15 3C16.6569 3 18 4.34315 18 6C18 7.65685 16.6569 9 15 9C13.9565 9 13.0375 8.46725 12.5 7.6589" stroke="#f5f5f5" stroke-width="1.7280000000000002"/>
+                <ellipse cx="9" cy="17" rx="7" ry="4" stroke="#f5f5f5" stroke-width="1.7280000000000002"/>
+                <path opacity="0.5" d="M18 14C19.7542 14.3847 21 15.3589 21 16.5C21 17.5293 19.9863 18.4229 18.5 18.8704" stroke="#f5f5f5" stroke-width="1.7280000000000002" stroke-linecap="round"/>
+            </g>
+        </svg>
+        <button className='button-login'><a href="#login">Ingresa</a></button>
+        <button className='button-servicios'><a href="#servicios">Servicios</a></button>
+        <button className='button-blog'><a href="#blog">Blog</a></button>
+        <button className='button-nosotros'><a href="#nosotros">Nosotros</a></button>
+        <button className='button-contacto'><a href="#contacto">Contacto</a></button>
+    </nav>
+};
+
+export default Navbar;
--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import Navbar from '../../Components/Navbar/Navbar';
+import './LandingModule.css'
 
 const Landing = () => {
   return (
     <div>
-          <div>
-              <Link to={'/home'}>
-              </Link>
-      </div>
+      <Navbar/>
+      <main>
+        <section id="introduccion" className='introduccion'>
+          <h3>Aqui está la seccion introducción</h3>
+        </section>
+        <hr />
+        <section id="anuncios" className='anuncios'>
+          <h3>Aqui está la sección Anuncios</h3>
+        </section>
+        <hr />
+        <section id="login" className='login'>
+          <h3 >Aqui está la seccioón login</h3>
+        </section>
+        <hr />
+        <section id="servicios" className='servicios'>
+          <h3 >Aqui está la seccioón servicios</h3>
+        </section>
+        <hr />
+        <section id="blog">
+          <h3 >Aqui está la seccioón blog</h3>
+        </section>
+        <hr />
+        <section id="nosotros">
+          <h3 >Aqui está la seccioón nosotros</h3>
+        </section>
+        <hr />
+        <section id="contacto">
+          <h3 >Aqui está la seccioón contacto</h3>
+        </section>
+        <hr />
+        <footer>
+          <h3>Aquí está el footer</h3>
+        </footer>
+      </main>
     </div>
   );
 };
