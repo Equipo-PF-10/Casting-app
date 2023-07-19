de2a2aa7d76ac5789c57a207be3020b3180103e1
Pedro Cavataio <pedrocavataio@gmail.com>
2023-07-19 16:11:04 -0300

    handler y controller

---
+++ server/src/controllers/companiesController.js
@@ -0,0 +1,12 @@
+const Empresa = require("../models/Empresa");
+
+const getAllCompanies = async (req, res) => {
+  try {
+    const empresas = await Empresa.findAll();
+    res.status(200).json(empresas);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener las empresas de la base de datos: " + error });
+  }
+};
+
+module.exports = { getAllCompanies };
