0343caaf9ea5b9deee418d65a5e8abe4584fe5fb
santiocampo1 <sanntiocampo@gmail.com>
2023-07-19 10:20:01 -0300

    Modificación del controller getTalents, para que muestre los datos correctos

--- server/src/importData/usersData.js
+++ server/src/importData/usersData.js
@@ -2,6 +2,33 @@ const axios = require("axios");
 const { Talento } = require("../db");
 const { v4: uuidv4 } = require("uuid");
 
+let habilities = [
+  "Actuación",
+  "Animador/a",
+  "Bailarín/a",
+  "Blogger",
+  "Cantante",
+  "DJ",
+  "Influencer",
+  "Locutor/a",
+  "Mago/a",
+  "Músico/a",
+  "Modelo",
+  "Presentador/a",
+  "Promotor/a",
+];
+
+const getRandomHabilities = (habilities) => {
+  const minHabilities = 1;
+  const maxHabilities = Math.min(3, habilities.length);
+  const numberOfHabilities =
+    Math.floor(Math.random() * (maxHabilities - minHabilities + 1)) +
+    minHabilities;
+
+  const shuffledHabilities = habilities.sort(() => 0.5 - Math.random());
+  return shuffledHabilities.slice(0, numberOfHabilities);
+};
+
 const getUsersData = async () => {
   try {
     const response = await axios("https://randomuser.me/api/?results=100");
@@ -11,20 +38,24 @@ const getUsersData = async () => {
       usersData.map(async (user) => {
         const id = uuidv4();
         const name = `${user.name.first} ${user.name.last}`;
+        const { email } = user;
         const nationality = user.location.country;
         const ubication = user.location.city;
+        const password = user.login.password;
+        const image = user.picture.large;
+        const contact = [user.phone, user.cell];
+        const hability = getRandomHabilities(habilities);
 
         const talent = await Talento.create({
           id,
           name,
-          email: "@gmail.com",
-          password: "1234",
-          image: "url.png",
-          hability: "Actuación",
+          email,
+          password,
+          image,
           nationality,
           ubication,
-          talent: "",
-          contact: [],
+          hability,
+          contact,
         });
 
         return talent;
