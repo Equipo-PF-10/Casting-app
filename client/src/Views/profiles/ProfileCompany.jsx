59d0452b9921dbf04f14b5cdb5dde5b4ade30233
jesusre890 <jesusre890@gmail.com>
2023-07-20 00:39:02 -0300

    maquetado terminado, faltan estilos

--- client/src/Views/profiles/ProfileCompany.jsx
+++ client/src/Views/profiles/ProfileCompany.jsx
@@ -4,12 +4,13 @@ import style from "./ProfileCompany.module.css";
 const profileCompany = () => {
   return (
     <div className={style.containerGeneral}>
+      {/*carta con foto y descripcion*/}
       <div className={style.cardContainer}>
         <div className={style.image}>
           <img src="" alt="" />
         </div>
-        <div>
-          <h2 className={style.nombre}>Nombre Apellido</h2>
+        <div className={style.textoCard}>
+          <h2 className={style.nombre}>Empresa</h2>
           <p className={style.titulo}>Titulo</p>
           <p className={style.descripcion}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
@@ -18,6 +19,69 @@ const profileCompany = () => {
           </p>
         </div>
       </div>
+      {/*carta de orientacion*/}
+      <div className={style.orientacion}>
+        <h3>Orientacion Artistica</h3>
+        <div className={style.habilidades}>
+          <div className={style.habilidad}>
+            <h4>Deporte</h4>
+          </div>
+          <div className={style.habilidad}>
+            <h4>Arte</h4>
+          </div>
+          <div className={style.habilidad}>
+            <h4>Humor</h4>
+          </div>
+        </div>
+      </div>
+      {/*carta de imagenes*/}
+      <div className={style.imagenesCard}>
+        <div>
+          <p>ImagenUno</p>
+        </div>
+        <div>
+          <p>ImagenDos</p>
+        </div>
+        <div>
+          <p>ImagenTres</p>
+        </div>
+        <div>
+          <p>ImagenCuatro</p>
+        </div>
+      </div>
+      {/*carta de proyectos*/}
+      <div className={style.proyectosCard}>
+        <div className={style.textoProyectos}>
+          <h3>Proyectos</h3>
+          <p>proyecto 1</p>
+          <p>proyecto 2</p>
+          <p>proyecto 3</p>
+        </div>
+          </div>
+          {/*carta de trabajos postulados*/}
+          <div>
+              <div>
+                  <h3>Trabajos postulados</h3>
+                  <p>trabajo 1</p>
+                  <p>trabajo 2</p>
+                  <p>trabajo 3</p>
+                  <p>trabajo 4</p>
+              </div>
+          </div>
+          {/*carta de contacto*/}
+          <div>
+              <div className={style.contactos}>
+                  <h3>Contactos</h3>
+              </div>
+              <div className={style.listaContactos}>
+                  <p>telefono con svg</p>
+                  <p>facebook con svg</p>
+                  <p>mail con svg</p>
+                  <p>instagram con svg</p>
+                  <p>twiter con svg</p>
+                  <p>pagina web con svg</p>
+              </div>
+          </div>
     </div>
   );
 };
