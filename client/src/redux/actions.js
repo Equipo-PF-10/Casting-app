923c3d76973a7ed6e4958b391f10142bd726eb02
Edward Vera <edwardismael1@gmail.com>
2023-07-20 16:50:33 -0300

    Cambio en actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,11 +2,10 @@ export const REGISTER_MODEL = "REGISTER_MODEL";
 export const ERROR = "ERROR";
 
 export const register_model = (payload) => {
-    let endpoint = "https://casting-app-server.onrender.com/talents/register"; 
+    let endpoint = "http://localhost:5173/talents/register"; 
     return async (dispatch) => {
        try {
           const response = await axios.post(endpoint, payload);
-          console.log(response.data);
           return dispatch({
              type: "REGISTER_MODEL",
              payload: response.data
