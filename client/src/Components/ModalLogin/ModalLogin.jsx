4a9845738529817e4953e675d4e864305e5acce3
Edward Vera <edwardismael1@gmail.com>
2023-07-21 10:48:04 -0300

    cambios en el modal

--- client/src/Components/ModalLogin/ModalLogin.jsx
+++ client/src/Components/ModalLogin/ModalLogin.jsx
@@ -1,32 +1,40 @@
 import styles from "./ModalLogin.module.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { modal_login } from "../../redux/actions";
+import { useDispatch } from "react-redux";
 
 export default function ModalLogin() {
-  //En este componente cierro el Modal
+  //Este componente se muestra cuando modalInLogin === true
   let modal = useState((state) => state.modalInLogin);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handler_click_talent = () => {
+    navigate('/model/register'); 
+  }
+
+  const handler_click_company = () => {
+    navigate('/company/register'); 
+  }
+
+  const handler_click_close = () => {
+    const close = "isClosed";
+    dispatch(modal_login(close));
+  }
 
   return (
     <div>
-      {modal ? (
         <div className={styles.containerModalOpened}>
-          <div className={styles.modalOpenedError}>
-            <button onClick={onClickCloseError} className={styles.delete}>
-              X
-            </button>
+          <div className={styles.modalOpened}>
+            <button onClick={handler_click_close} className={styles.delete}> X </button>
 
             <h2>Selecciona un tipo de Registro</h2>
 
-            <button>Talento</button>
-            <button>Empresa</button>
+            <button onClick={handler_click_talent}>Talento</button>
+            <button onClick={handler_click_company}>Empresa</button>
           </div>
         </div>
-      ) : (
-        <div className={styles.containerModalClosed}>
-          <div className={styles.modalClosed}>
-            <h2>Selecciona un tipo de Registro</h2>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
