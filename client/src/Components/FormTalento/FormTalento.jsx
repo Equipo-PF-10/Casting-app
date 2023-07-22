2e4f6c56d9ff1438519fd289fb5519150694f65a
DrownUnity <pabloesaaray@gmail.com>
2023-07-22 14:50:28 -0500

    Form update v2

--- client/src/Components/FormTalento/FormTalento.jsx
+++ client/src/Components/FormTalento/FormTalento.jsx
@@ -76,34 +76,34 @@ const FormTalento = () => {
             <form action="" className={Styles.form} method="POST" onSubmit={hanldeSubmit}>
                 <div className={Styles.div}>
                 <h1>Registro</h1>
-                    <article className={Styles.art}>
+                    <coolinputicle className={Styles.coolinput}>
+                        <label htmlFor="name" className={Styles.text}>Nombre Completo</label>
                         <input type="text" name="name" id="name" value={input.name} onChange={handleChange} required/>
-                        <label htmlFor="name">Nombre Completo</label>
                         <p className={error.name ? Styles.error : ""}>{error.name ? error.name : null}</p>
-                    </article>
+                    </coolinputicle>
 
-                    <article className={Styles.art}>
-                        <input type="password" name="password" id="password" value={input.password} onChange={handleChange} required/>
+                    <coolinputicle className={Styles.coolinput}>
                         <label htmlFor="password">Contraseña</label>
+                        <input type="password" name="password" id="password" value={input.password} onChange={handleChange} required/>
                         <p className={error.password ? Styles.error : ""}>{error.password ? error.password : null}</p>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="password" name="passwordConfirm" id="passwordConfirm" onChange={handleChange} required/>
                         <label htmlFor="passwordConfirm">Confirma Contraseña</label>
                         <p className={error.passwordConfirm ? Styles.error : ""}>{error.passwordConfirm ? error.passwordConfirm: null}</p>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" name="email" id="email" value={input.email} onChange={handleChange} required/>
                         <label htmlFor="email">Email</label>
                         <p className={error.email ? Styles.error : ""}>{error.email ? error.email : null}</p>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" name="emailConfirm" id="emailConfirm" onChange={handleChange} required/>
                         <label htmlFor="emailConfirm">Confirmar Email</label>
                         <p className={error.emailConfirm ? Styles.error : ""}>{error.emailConfirm ? error.emailConfirm: null}</p>
-                    </article>
-                    <article className={Styles.coolinput}>
-                                <label htmlFor="" className={Styles.text}>Orientación Artística</label>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
+                                <label htmlFor="" className={Styles.text}>Orientación coolinputística</label>
                                 <Select
                                 isMulti 
                                 options={optionshabilityRequired}
@@ -112,25 +112,25 @@ const FormTalento = () => {
                                 onChange={handleChangeSelect}
                                 name="habilityRequired"/>
                                 <p className={error.habilityRequired ? Styles.error : ""}>{error.habilityRequired ? error.habilityRequired : null}</p>
-                            </article> 
-                    <article className={Styles.art}>
+                            </coolinputicle> 
+                    <coolinputicle className={Styles.coolinput}>
                         
-                    </article>
+                    </coolinputicle>
 
                 </div>
                 <div className={Styles.div}>
-                <article className={Styles.img}>
+                <coolinputicle className={Styles.img}>
                         <svg viewBox="0 0 334 334" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g filter="url(#filter0_d_100_143)">
-                        <g clip-path="url(#clip0_100_143)">
+                        <g clipPath="url(#clip0_100_143)">
                         <rect x="5" y="2" width="324" height="324" rx="162" fill="#F5F5F5"/>
                         <rect x="102.056" y="53.9554" width="129.889" height="129.889" rx="64.9443" fill="#4B31A1"/>
                         <rect x="-76.1807" y="218.12" width="486.361" height="486.361" rx="243.18" fill="#4B31A1"/>
                         </g>
                         </g>
                         <defs>
-                        <filter id="filter0_d_100_143" x="0.670379" y="0.556793" width="332.659" height="332.659" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                        <filter id="filter0_d_100_143" x="0.670379" y="0.556793" width="332.659" height="332.659" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
                         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
                         <feMorphology radius="1.44321" operator="dilate" in="SourceAlpha" result="effect1_dropShadow_100_143"/>
                         <feOffset dy="2.88641"/>
@@ -145,76 +145,76 @@ const FormTalento = () => {
                         </clipPath>
                         </defs>
                         </svg>
-                    </article>
-                    <article className={Styles.artImg}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinputImg}>
                         <input type="file" name="img" value={input.img} required onChange={handleChange}/>
                         <label htmlFor="">Subir Imágen</label>
-                    </article>
+                    </coolinputicle>
 
                     <h4>Características</h4>
-                    <article className={Styles.art1}>
+                    <coolinputicle className={Styles.coolinput1}>
                         <label htmlFor="">Piel</label>
                         <select name="" id="">
                             <option value="">Tipos</option>
                         </select>
-                    </article>
-                    <article className={Styles.art1}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput1}>
                         <label htmlFor="">Contextura</label>
                         <select name="" id="">
                             <option value="">Tipos</option>
                         </select>
-                    </article>
+                    </coolinputicle>
                     <h4>Medidas</h4>
-                    <article className={Styles.art1}>
+                    <coolinputicle className={Styles.coolinput1}>
                         <label htmlFor="">Altura</label>
                         <input type="number"  onChange={handleChange}/>
-                    </article>
-                    <article className={Styles.art1}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput1}>
                         <label htmlFor="">Peso</label>
                         <input type="number" onChange={handleChange} />
-                    </article >
+                    </coolinputicle >
                     <h4>Tallas</h4>
-                    <article className={Styles.art1}>
+                    <coolinputicle className={Styles.coolinput1}>
                         <label htmlFor="">Piel</label>
                         <input type="number"  onChange={handleChange}/>
-                    </article>
-                    <article className={Styles.art1}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput1}>
                         <label htmlFor="">Contextura</label>
                         <input type="number"  onChange={handleChange}/>
-                    </article>      
+                    </coolinputicle>      
                 </div>
                 <div className={Styles.div}>
-                    <article className={Styles.art}>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" name="num" id="num" value={input.num} onChange={handleChange}/>
                         <label htmlFor="num">Número Telefónico</label>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" name="facebook" value={input.facebook} onChange={handleChange}/>
                         <label htmlFor="">Facebook</label>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" name="instagram" value={input.instagram}  onChange={handleChange}/>
                         <label htmlFor="">Instagram</label>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" name="twitter" value={input.twitter} onChange={handleChange}/>
                         <label htmlFor="">Twitter</label>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" id="location" name="location" value={input.location} onChange={handleChange} required/>
                         <label htmlFor="location">Ubicación</label>
                         <p className={error.location ? Styles.error : ""}>{error.location ? error.location : null}</p>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text"  name="nacionalidad" id="nacionalidad" value={input.nacionalidad} onChange={handleChange} required/>
                         <label htmlFor="">Nacionalidad</label>
                         <p className={error.nacionalidad ? Styles.error : ""}>{error.nacionalidad ? error.nacionalidad : null}</p>
-                    </article>
-                    <article className={Styles.art}>
+                    </coolinputicle>
+                    <coolinputicle className={Styles.coolinput}>
                         <input type="text" name="dni" id="dni" value={input.dni} onChange={handleChange} required/>
                         <label htmlFor="dni">Documento de Identidad</label>
                         <p className={error.dni ? Styles.error : ""}>{error.dni ? error.dni : null}</p>
-                    </article>
+                    </coolinputicle>
                     <button type="submit">Enviar Datos</button>
                 </div>
             </form>
