e34cdd3a3596ce77225164f679c24a691a918216
jesusre890 <jesusre890@gmail.com>
2023-07-24 03:10:54 -0300

    rederizado en talentsSearch

--- client/src/Views/Searches/TalentComponent/CardJobs.jsx
+++ client/src/Views/Searches/TalentComponent/CardJobs.jsx
@@ -1,14 +1,26 @@
-import React from "react";
-import style from '../TalentSearch.module.css'
-import styleCard from './CardJobs.module.css'
+import React, { useEffect } from "react";
+//import style from '../TalentSearch.module.css'
+import styleCard from "./CardJobs.module.css";
+import { get_company_by_id } from "../../../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
 
-const CardJobs = () => {
+const CardJobs = ({ event }) => {
+  const idCompany = event.idEmpresa;
+  //console.log(idCompany);
+  const companyData = useSelector((state) => state.companyDetail);
+  //console.log(companyData);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(get_company_by_id(idCompany));
+  }, [dispatch, idCompany]);
+
   return (
     <div className={styleCard.containerCardJobs}>
-      <h2>Trabajo - Evento</h2>
-      <h5>Locación</h5>
-      <h5>Empresa</h5>
-      <div className={style.brLinea}></div>
+      <h2>{event.name}</h2>
+      <h5>{event.ubication}</h5>
+      <h5>{companyData.name}</h5>
+      {/*<div className={style.brLinea}></div>*/}
     </div>
   );
 };
--- client/src/Views/Searches/TalentComponent/Detail.jsx
+++ client/src/Views/Searches/TalentComponent/Detail.jsx
@@ -1,40 +1,23 @@
-import React from 'react'
-import style from './DetailComp.module.css'
+import React from "react";
+import style from "./DetailComp.module.css";
 
-const Detail = () => {
+const Detail = ({ details }) => {
+  const { name, country, description, expirationDate, industryMain } = details;
+  //console.log(name);
+  
+
   return (
     <div className={style.containerDetail}>
-      <h2>Nombre Apellido</h2>
-      <h5>Locación</h5>
-      <h5>Empresa</h5>
-      <p className={style.textoDetail}>
-        Fusce ac lacus ac diam ullamcorper egestas ut nec velit. Duis facilisis
-        urna vel sapien elementum euismod. Phasellus aliquet feugiat pretium.
-        Etiam id dignissim dui. Maecenas ex velit, fringilla non scelerisque ut,
-        ullamcorper vel tellus. In lacinia tincidunt lorem et ullamcorper. Etiam
-        nec facilisis felis.imperdiet. Fusce ac lacus ac diam ullamcorper
-        egestas ut nec velit. Duis facilisis urna vel sapien elementum euismod.
-        Phasellus aliquet feugiat pretium. Etiam id dignissim dui. Maecenas ex
-        velit, fringilla non scelerisque ut, ullamcorper vel tellus. In lacinia
-              tincidunt lorem et ullamcorper.
-              Etiam nec facilisis felis. Donec magna
-        ex, interdum non interdum ut, fermentum a lorem. Donec quis neque eu
-        magna sollicitudin fermentum sed dapibus elit. Etiam molestie accumsan
-        velit pharetra imperdiet.
-      </p>
-      <p className={style.textoDetail}>
-        Fusce ac lacus ac diam ullamcorper egestas ut nec velit. Duis facilisis
-        urna vel sapien elementum euismod. Phasellus aliquet feugiat pretium.
-        Etiam id dignissim dui. Maecenas ex velit, fringilla non scelerisque ut,
-        ullamcorper vel tellus. In lacinia tincidunt lorem et ullamcorper. Etiam
-        nec facilisis felis.imperdiet. Fusce ac lacus ac diam ullamcorper
-        egestas ut nec velit. Duis facilisis urna vel sapien elementum euismod.
-      </p>
+      <h2>{name}</h2>
+      <h5>{country}</h5>
+      <h5>Industria: {industryMain}</h5>
+      <p>{expirationDate}</p>
+      <p className={style.textoDetail}>{description}</p>
       <div className={style.conteinerConectar}>
         <button className={style.postularme}>Postularme</button>
       </div>
     </div>
   );
-}
+};
 
-export default Detail
+export default Detail;
