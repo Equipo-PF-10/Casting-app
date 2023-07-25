04e3aeda9a73cfba223819de87d285d1340ddff5
JulioACegarraC <juliocegarra19@gmail.com>
2023-07-25 13:54:01 -0500

    m

--- server/src/routes/companies/companyRouter.js
+++ server/src/routes/companies/companyRouter.js
@@ -1,18 +1,18 @@
 const { Router } = require("express");
-const { handleSearchByLocation, handleCreateCompany, handlerGetAllCompanies, handleUpdateCompanyById, handleGetCompanyById, handlerDeleteCompanyById } = require("../../handlers/companies/companiesHandler");
+const { handlerSearchByLocation, handlerCreateCompany, handlerGetAllCompanies, handlerUpdateCompanyById, handlerGetCompanyById, handlerDeleteCompanyById } = require("../../handlers/companies/companiesHandler");
 
 const companyRouter = Router();
 //* Esta ruta busca todas las empresas por una ubicación especifica.
-companyRouter.get("/location", handleSearchByLocation);
+companyRouter.get("/location", handlerSearchByLocation);
 
 //* Esta ruta registra una nueva compañia en la base de datos.
-companyRouter.post("/register", handleCreateCompany);
+companyRouter.post("/register", handlerCreateCompany);
 
 //* Esta ruta busca una compañia por id en la base de datos.
-companyRouter.get("/:id", handleGetCompanyById);
+companyRouter.get("/:id", handlerGetCompanyById);
 
 //* Esta ruta actualiza una compañia por id en la base de datos.
-companyRouter.put("/:id", handleUpdateCompanyById);
+companyRouter.put("/:id", handlerUpdateCompanyById);
 
 //* Esta ruta elimina por completo una compañia por id en la base de datos.
 //! debemos modificar para ejecutar el borrado logico
