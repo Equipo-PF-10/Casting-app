bfcb62d6fd77819c2c5506db55e618ff3c040613
santiocampo1 <sanntiocampo@gmail.com>
2023-07-26 16:54:23 -0300

    Eventos creados en la database

--- server/index.js
+++ server/index.js
@@ -3,15 +3,30 @@ const { conn } = require("./src/db");
 const { PORT } = process.env;
 const talentsFromApi = require("./src/data/talentsFromApi");
 const companiesFromApi = require("./src/data/companiesFromApi");
-//const eventsMocks = require("./src/data/eventsMocks");
+const eventsMocks = require("./src/data/eventsMocks");
 
 // Syncing all the models at once.
 // force is to reset my database
-conn.sync({ force: true }).then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log("listening at", PORT);
-  });
-  companiesFromApi();
-  talentsFromApi();
-  // eventsMocks();
-});
+const syncDB = async () => {
+  try {
+    await conn.sync({ force: true });
+    console.log("Database synchronized successfully.");
+    
+    // Primero crear empresas
+    await companiesFromApi();
+    
+    // Luego crear talentos
+    await talentsFromApi();
+    
+    // Finalmente, crear eventos
+    await eventsMocks();
+
+    app.listen(process.env.PORT, () => {
+      console.log("Listening at", PORT);
+    });
+  } catch (error) {
+    console.error("Error syncing database:", error);
+  }
+};
+
+syncDB();
--- server/src/data/eventsMocks.js
+++ server/src/data/eventsMocks.js
@@ -1,47 +1,27 @@
-const { Evento } = require("../db");
-const eventsMosckData = require("./eventsMocks.json");
+const { Event, Company } = require("../db");
+const eventsFullData = require("./eventsMocks.json");
 
 const eventsMocks = async () => {
   try {
-    const eventsData = eventsMosckData.results;
+    let events = []
 
-    const allEvents = await Promise.all(
-      eventsData.map(async (event) => {
-        const {
-          name,
-          image,
-          shortDescription,
-          creationDate,
-          expirationDate,
-          changeDate,
-          detail,
-          active,
-          ubication,
-          habilityRequired,
-          contact,
-          habilitySalary,
-          CompanyId
-        } = event;
+    const eventsMapeados = eventsFullData.events.map((event) => {
+      const ev = {}
 
-        const createdEvent = await Evento.create({
-          name,
-          image,
-          shortDescription,
-          creationDate,
-          expirationDate,
-          changeDate,
-          detail,
-          active,
-          ubication,
-          habilityRequired,
-          contact,
-          habilitySalary,
-          CompanyId
-        });
-        return createdEvent;
-      })
-    );
-    return allEvents;
+      ev.name = event.name
+      ev.image = event.image
+      ev.shortDescription = event.shortDescription
+      ev.detail = event.detail
+      ev.active = event.active
+      ev.ubication = event.ubication
+      ev.habilityRequired = event.habilityRequired
+      ev.habilitySalary = event.habilitySalary
+      ev.expirationDate = event.expirationDate
+      ev.contact = event.contact
+      events.push(ev)
+    })
+
+    const savedEvents = await Event.bulkCreate(events)
   } catch (error) {
     throw new Error(error.message);
   }
