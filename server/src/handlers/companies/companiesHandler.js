cc56f3a86a526e7063babdb576a9a63a2dbc5cb8
santiocampo1 <sanntiocampo@gmail.com>
2023-07-26 13:11:04 -0300

    a

--- server/src/handlers/companies/companiesHandler.js
+++ server/src/handlers/companies/companiesHandler.js
@@ -56,9 +56,9 @@ const handlerCreateCompany = async (req, res) => {
 
   try {
     const created = await createCompany(email, password);
-    res.status(200).json(created);
+    res.status(200).send("La companía ha sido creada con éxito.");
   } catch (error) {
-    res.status(400).json(error.message);
+    res.status(400).send(error.message);
   }
 };
 
