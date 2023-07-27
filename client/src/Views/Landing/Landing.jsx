a28971f050919f6ad6f21acba78b92c1e658aad8
Pedro Cavataio <pedrocavataio@gmail.com>
2023-07-26 23:57:50 -0300

    email finalizado falta diseño

--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -2,9 +2,27 @@ import React from 'react';
 // import ChartComponent from '../../Components/graficos/chart.jsx';
 import Adds from '../../Components/Adss/Adds.jsx';
 import Navbar from '../../Components/Navbar/Navbar.jsx';
-import './LandingModule.css'
+ import './LandingModule.css'
+
 
 const Landing = () => {
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    const response = await fetch(event.target.action, {
+      method: event.target.method,
+      body: form,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      event.target.reset();
+      alert('Gracias por contactarte con CastingAPP, te responderemos a la brevedad');
+    }
+  };
 
   return (
     <>
@@ -95,17 +113,56 @@ const Landing = () => {
           <div class="card-nosotros"></div>
         </section>
         <section id="contacto" className='contacto'>
-          <p>!!! Tienes algo que quisieras contarnos...? <b>Contactanos</b></p>
-          <form action="mailto:castingapp.pf.10@gmail.com" method="post" encType="text/plain" autoComplete='off'>
-            Nombre:<br/>
-            <input type="text" name="nombre"/><br/>
-            Correo electrónico:<br/>
-            <input type="text" name="correo"/><br/>
-            Comentario:<br/>
-            <textarea type="text" name="comentario" size="50" className='input-comentario'/><br/><br/>
-            <input type="submit" value="Enviar"/>
-            <input type="reset" value="Borrar"/>
-          </form>
+        
+<div className="container p-5">
+      <div className="row justify-content-center">
+        <div className="col-sm-12 col-md-8 col-lg-6">
+          <div className="card">
+            <div className="card-header text-center">
+              <h1>Formulario de contacto</h1>
+            </div>
+            <div className="card-body">
+              <form action="https://formspree.io/f/xoqovoyp" method="POST" onSubmit={handleSubmit}>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    name="Name"
+                    placeholder="Nombre"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    name="email"
+                    placeholder="Correo electrónico"
+                    className="form-control"
+                  />
+                </div>
+
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    name="phone"
+                    placeholder="Teléfono"
+                    className="form-control"
+                  />
+                </div>
+                <div className="mb-3">
+                  <textarea className="form-control" name="message" rows="6" placeholder="Mensaje"></textarea>
+                </div>
+                <div className="text-center">
+                  <button className="btn btn-primary w-100">Enviar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
         </section>
       </main>
       <footer>
