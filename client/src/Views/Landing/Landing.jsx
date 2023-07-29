fe03a90fe028f275713a889769256cd3fa01f59e
Pedro Cavataio <pedrocavataio@gmail.com>
2023-07-29 15:20:36 -0300

    se mejora diseño formulario de envio email en landing y se agregan archivos para enviar email personalizados

--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -112,58 +112,75 @@ const Landing = () => {
           <h3 >Aqui está la seccioón nosotros</h3>
           <div class="card-nosotros"></div>
         </section>
+        
+
         <section id="contacto" className='contacto'>
         
-<div className="container p-5">
-      <div className="row justify-content-center">
-        <div className="col-sm-12 col-md-8 col-lg-6">
-          <div className="card">
-            <div className="card-header text-center">
-              <h1>Formulario de contacto</h1>
+        <div className="container p-5">
+              <div className="row justify-content-center">
+                <div className="col-sm-12 col-md-8 col-lg-6">
+                  <div className="cardMail">
+                    <div className="card-header text-center">
+                    <h3>Formulario de contacto</h3>
+                    </div>
+                    <div className="card-body">
+                      <form action="https://formspree.io/f/xoqovoyp" method="POST" onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                          <input
+                            type="text"
+                            name="Name"
+                            placeholder="Nombre"
+                            className="form-control"
+                            
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <input
+                            type="text"
+                            name="email"
+                            placeholder="Correo electrónico"
+                            className="form-control"
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <input
+                            type="text"
+                            name="phone"
+                            placeholder="Teléfono"
+                            className="form-control"
+                          />
+                        </div>
+
+                        <div className="mb-3">
+                          <textarea className="form-control" name="message" rows="3" placeholder="Mensaje"></textarea>
+                        </div>
+
+                        <div className="text-center">
+                          <button className="btn btn-primary w-100">Enviar</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="card-body">
-              <form action="https://formspree.io/f/xoqovoyp" method="POST" onSubmit={handleSubmit}>
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    name="Name"
-                    placeholder="Nombre"
-                    className="form-control"
-                  />
-                </div>
-
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    name="email"
-                    placeholder="Correo electrónico"
-                    className="form-control"
-                  />
-                </div>
-
-                <div className="mb-3">
-                  <input
-                    type="text"
-                    name="phone"
-                    placeholder="Teléfono"
-                    className="form-control"
-                  />
-                </div>
-                <div className="mb-3">
-                  <textarea className="form-control" name="message" rows="6" placeholder="Mensaje"></textarea>
-                </div>
-                <div className="text-center">
-                  <button className="btn btn-primary w-100">Enviar</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
         </section>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+
       </main>
       <footer>
         <h3>Aquí está el footer</h3>
