4e8df6527d2fa998753de4e61eaf0f8c3aa9d8d0
DrownUnity <pabloesaaray@gmail.com>
2023-08-02 10:45:52 -0500

    HomeTalents y Reportes

--- server/index.js
+++ server/index.js
@@ -12,14 +12,16 @@ const syncDB = async () => {
     await conn.sync({ force: true });
     console.log("Database synchronized successfully.");
 
+    //! Comentar al terminar
+
     // Primero crear empresas
-    // await companiesFromApi();
+    await companiesFromApi();
 
     // Luego crear talentos
-    // await talentsFromApi();
+    await talentsFromApi();
 
     // Finalmente, crear eventos
-    //await eventsMocks();
+    await eventsMocks();
 
     app.listen(process.env.PORT, () => {
       console.log("Listening at", PORT);
