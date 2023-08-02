e69adbf1cb7bde9b776403ed7531a299a44c116e
DrownUnity <pabloesaaray@gmail.com>
2023-08-02 14:23:29 -0500

    Correción Evento

--- client/src/Components/Cloudinary/Cloudinary.jsx
+++ client/src/Components/Cloudinary/Cloudinary.jsx
@@ -1,44 +1,50 @@
 import axios from "axios";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import style from "./Cloudinary.module.css"
 import { useDispatch } from "react-redux";
 import { getUrlImage } from "../../redux/actions";
 
 const Cloudinary = () => {
 
-    const [image, setImage] = useState("")
-    const dispatch = useDispatch();
+  const [image, setImage] = useState("")
+  const dispatch = useDispatch();
 
-    const uploadPreset = "casting_app"
-    const cloudName = "dntrnqcxe";
+  const uploadPreset = "casting_app"
+  const cloudName = "dntrnqcxe";
 
-    const URL = `https://api.cloudinary.com/v1_1/${cloudName}/upload`
+  const URL = `https://api.cloudinary.com/v1_1/${cloudName}/upload`
 
-    const submitImage = async () => {
-      if (image === "") {
-        console.log("No se ha seleccionado una imagen.");
-        return;
-      }
+  const submitImage = async () => {
+    if (image === "") {
+      console.log("No se ha seleccionado una imagen.");
+      return;
+    }
   
-      const formData = new FormData();
-      formData.append("file", image);
-      formData.append("upload_preset", uploadPreset);
-      formData.append("cloud_name", cloudName);
+  const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_preset", uploadPreset);
+    formData.append("cloud_name", cloudName);
   
-      try {
-        const response = await axios.post(URL, formData);
-        const responseData = response.data;
-        const imageUrl = responseData.url;
-        dispatch(getUrlImage(imageUrl));
-      } catch (error) {
-        console.log({ error });
-      }
-    };
+    try {
+      const response = await axios.post(URL, formData);
+      const responseData = response.data;
+      const imageUrl = responseData.url;
+      dispatch(getUrlImage(imageUrl));
+    } catch (error) {
+      console.log({ error });
+    }
+  };
   
     const handleFileChange = (event) => {
-      setImage(event.target.files[0]);
-      submitImage(); 
+      const file = event.target.files[0];
+      setImage(file)
     };
+
+    useEffect(() => {
+      if(image){
+        submitImage()
+      } 
+    })
   
     return(
         <section className={style.section}>
