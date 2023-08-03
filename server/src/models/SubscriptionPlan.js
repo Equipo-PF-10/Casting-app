76b4b24fabc53d3e3f98a816d4001f49968ed5c1
SGrommelt <onehappeeshala@gmail.com>
2023-08-03 07:40:26 -0600

    Subscription plans Models, Routes, Handlers, Controllers.

---
+++ server/src/models/SubscriptionPlan.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  return sequelize.define("SubscriptionPlan",{
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+
+      name: {
+        type: DataTypes.ENUM("PREMIUM", "PRO", "OTHER"),
+        allowNull: false,
+      },
+
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+};
