488c5bd64ea1b2399c398125f9f4a1360067d84c
jesusre890 <jesusre890@gmail.com>
2023-08-06 01:20:29 -0300

    hired-talent con info

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -44,7 +44,7 @@ import {
 const initialState = {
   talents: [],
   talentById: {},
-  hiredByCompany: {},
+  hiredByCompany: [],
   postulatedTalentsByEvent: [],
   postulatedTalentsByEventFiltered: [],
   allEvents: [],
