42e067f519d21e7ac860ced829e467e61dacc369
Agustin <aguvargass3@gmail.com>
2023-08-07 17:04:38 -0300

    Preloader Completado

--- client/src/Components/Preloader/Preloader.jsx
+++ client/src/Components/Preloader/Preloader.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import styles from './Preloader.module.css'; // Asegúrate de que el archivo CSS esté importado correctamente
+import styles from './Preloader.module.css'; // Asegúrate de tener importado correctamente tu archivo de estilos
 
-const Loader = () => {
+const Preloader = () => {
   return (
-    <div className={styles.container}>
-    <div className={styles.loader}>
-      <div className={styles.slider} style={{ '--i': 0 }}></div>
-      <div className={styles.slider} style={{ '--i': 1 }}></div>
-      <div className={styles.slider} style={{ '--i': 2 }}></div>
-      <div className={styles.slider} style={{ '--i': 3 }}></div>
-    </div>
+    <div className={styles.preloaderContainer}>
+      {/* Texto "Casting App" centrado arriba de la animación */}
+      <div className={styles.title}>Casting App</div>
+
+      {/* Contenedor de la animación */}
+      <div className={styles.container}>
+        <div className={styles.loader}>
+          <div className={styles.slider} style={{ '--i': 0 }}></div>
+          <div className={styles.slider} style={{ '--i': 1 }}></div>
+          <div className={styles.slider} style={{ '--i': 2 }}></div>
+          <div className={styles.slider} style={{ '--i': 3 }}></div>
+        </div>
+        
+        {/* Texto "Cargando..." centrado abajo de la animación */}
+        <div className={styles.loadingText}> Cargando..</div>
+      </div>
     </div>
   );
 };
 
-export default Loader;
+export default Preloader;
